3c45c9e675415dd289593170443cd4dbeb854d77
rekky1aws <killikomfr@gmail.com>
2024-12-06 01:54:04 +0100

    Locales routes
     + Route for locale homepage in HomeController.php
     + Global constant in services.yaml for supported locales.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,13 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController {
 
-    #[Route("/", name: "home")]
-    function index (Request $request) : Response {
+    #[Route("/")]
+    function indexNoLocale (Request $request) : Response {
+         return $this->redirectToRoute('home', ['_locale' => 'en']);
+    }
+
+    #[Route("/{_locale<%app.supported_locales%>}", name: "home")]
+    function indexLocale (Request $request) : Response {
          return $this->render('home/index.html.twig', [
             'controller_name' => "Home",
             'project_name' => "PinFX Tracker"
