b5eda5f13ac8374fbd9caf70dfb4eb16b51573b5
rekky1aws <killikomfr@gmail.com>
2024-12-08 19:16:08 +0100

    Header Logo links to app_home
     + Renamed home to app_home to be more consistent
     + Added a name to / route : app_home_noLocale
     + Link to app_home on header's logo click

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController {
 
-    #[Route("/")]
+    #[Route("/", name:"app_home_noLocale")]
     function indexNoLocale (Request $request) : Response {
-         return $this->redirectToRoute('home', ['_locale' => 'en']);
+         return $this->redirectToRoute('app_home', ['_locale' => 'en']);
     }
 
-    #[Route("/{_locale<%app.supported_locales%>}", name: "home")]
+    #[Route("/{_locale<%app.supported_locales%>}", name: "app_home")]
     function indexLocale (Request $request) : Response {
          return $this->render('home/index.html.twig', [
             'controller_name' => "Home",
