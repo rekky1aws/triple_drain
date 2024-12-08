47d5d24664b16a8b19adf8a2def2c8739f793985
rekky1aws <killikomfr@gmail.com>
2024-12-08 18:33:26 +0100

    Routes for rankings
    In RankingsController.php, added routes :
     + /rankings			rankings/index.html.twig
     + /rankings/global		rankings/global.html.twig
     + /rankings/category		rankings/categorySelection.html.twig
     + /rankings/category/{slug}	rankings/category.html.twig
     + /rankings/table		rankings/tableSelection.html.twig
     + /ranking/table/{slug}	rankings/table.html.twig

--- src/Controller/RankingsController.php
+++ src/Controller/RankingsController.php
@@ -23,4 +23,36 @@ class RankingsController extends AbstractController
             'controller_name' => 'Global Rankings',
         ]);
     }
+
+    #[Route('/rankings/catgeory', name: 'app_rankings_category_selection')]
+    public function categorySelection(): Response
+    {
+        return $this->render('rankings/categorySelection.html.twig', [
+            'controller_name' => 'Category Selection',
+        ]);
+    }
+
+    #[Route('/rankings/catgeory/{slug}', name: 'app_rankings_category')]
+    public function category(): Response
+    {
+        return $this->render('rankings/category.html.twig', [
+            'controller_name' => 'Category',
+        ]);
+    }
+
+    #[Route('/rankings/table', name: 'app_rankings_table_selection')]
+    public function tableSelection(): Response
+    {
+        return $this->render('rankings/tableSelection.html.twig', [
+            'controller_name' => 'Table Selection',
+        ]);
+    }
+
+    #[Route('/rankings/table/{slug}', name: 'app_rankings_table')]
+    public function table(): Response
+    {
+        return $this->render('rankings/table.html.twig', [
+            'controller_name' => 'Table',
+        ]);
+    }
 }
