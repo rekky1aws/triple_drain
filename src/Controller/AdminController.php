1bb3b234f2db862d6001a5b86203696707621235
rekky1aws <killikomfr@gmail.com>
2025-01-14 21:05:58 +0100

    Initialized Security and Users
     + Login form, template and controller
     + Logout controller
     + Register form, template and controller
     + Init admin page

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'Admin',
+        ]);
+    }
+
+    #[Route('/admin/insert_csv', name: 'app_admin_insertcsv')]
+    public function insertCSV (): Response
+    {
+        return $this->render('admin/insert_csv.html.twig',
+        [
+            'controller_name' => 'Admin - CSV Insertion'
+        ]);
+    }
+
+}
