218f2597038d002ec0da3fbb851d1e3c43cc4fc6
rekky1aws <killikomfr@gmail.com>
2025-01-27 22:37:39 +0100

    Still trying to make the damn form work

--- src/Controller/EditController.php
+++ src/Controller/EditController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\CsvImport;
 use App\Form\CsvImportType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,21 +23,35 @@ class EditController extends AbstractController
     }
 
     #[IsGranted('ROLE_EDITOR', message: 'You are not an editor.')]
-    #[Route('/edit/insert_csv', name: 'app_edit_insertcsv')]
-    public function insertCSV (Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/edit/check_csv', name: 'app_edit_checkcsv')]
+    public function checkCSV () : Response
     {
-        $form = $this->createForm(CsvImportType::class);
+        return $this->render('edit/index.html.twig', [
+            'controller_name' => 'Edition',
+        ]);
+    }
+
+    #[IsGranted('ROLE_EDITOR', message: 'You are not an editor.')]
+    #[Route('/edit/insert_csv', name: 'app_edit_insertcsv', methods: ["GET", "POST"])]
+    public function insertCSV (Request $request, EntityManagerInterface $entityManager): Response
+    {   
+        $csvImport = new CsvImport();
+        $form = $this->createForm(CsvImportType::class, $csvImport, [
+            'action' => $this->generateUrl('app_edit_checkcsv')
+        ]);
         $form->handleRequest($request);
-        
+
+        // dd($form->isSubmitted());
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // dd($form->get('file'));
+            dd($form->get('file'));
             $entityManager->flush();
         }
 
         return $this->render('edit/insert_csv.html.twig',
         [
             'controller_name' => 'Edition - CSV Insertion',
-            'form' => $form,
+            'form_data' => $form,
         ]);
     }
 }
--- src/Form/CsvImportType.php
+++ src/Form/CsvImportType.php
@@ -12,27 +12,31 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 // use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CsvImportType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            // ->add('importedAt', null, [
-            //     'widget' => 'single_text',
-            // ])
+            ->add('importedAt', null, [
+                'widget' => 'single_text',
+                'data' => new \DateTimeImmutable()
+            ])
             ->add('file', FileType::class, [
                 'mapped' => false,
                 // 'constraints' => [
                 //     new File("")
                 // ]
             ])
-            ->add('filename')
+            ->add('filename', TextType::class , [
+                'data' => date("Y-m-d")."_".date("H-m-s").".csv",
+            ])
             ->add('importedBy', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'username',
             ])
-            // ->add('send', SubmitType::class)
+            ->add('send', SubmitType::class)
         ;
     }
 
